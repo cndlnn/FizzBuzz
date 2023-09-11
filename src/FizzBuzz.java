e4c2a635ea3dbca77d231848017c117de8e125d9
Cindolinna <cindolinna.wong@mail.utoronto.ca>
2023-09-11 14:18:31 -0400

    refactored to extract helper method

--- src/FizzBuzz.java
+++ src/FizzBuzz.java
@@ -6,7 +6,7 @@ class FizzBuzz {
     public static void main(String[] args) {
 
         int i = 1;
-        while (i < 100) {
+        while (i < 100){
 
             // Find out which numbers divide i.
             i = doFizzBuzz(i);
